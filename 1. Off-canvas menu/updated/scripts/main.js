618e2db36b69194e171bdddc658af949a6836088
AlexMakowiecki <alex.makowiecki@gmail.com>
2024-03-14 22:35:29 -0300

    adding keyboard compatibility and screen responsiveness to updated version

--- 1. Off-canvas menu/updated/scripts/main.js
+++ 1. Off-canvas menu/updated/scripts/main.js
@@ -2,7 +2,40 @@ const main = document.querySelector('main')
 const toggleMenuButton = document.querySelector('.toggle-menu-button')
 const offCanvasMenu = document.querySelector('.off-canvas-menu')
 
+
+function getAnchorElements(element) {
+  anchorsArray = [...element.querySelectorAll('a')]
+  return anchorsArray
+}
+
 toggleMenuButton.addEventListener('click', _ => {
   main.classList.toggle('--off-canvas-menu-visible')
   offCanvasMenu.classList.toggle('--visible')
+  if (offCanvasMenu.classList.contains('--visible'))
+    offCanvasMenu.focus()
+  else
+    main.focus()
+})
+
+offCanvasMenu.addEventListener('keydown', (e) => {
+  const key = e.key.toLowerCase()
+  if ((key === 'escape') && (offCanvasMenu.classList.contains('--visible'))) {
+    offCanvasMenu.classList.toggle('--visible')
+    main.classList.toggle('--off-canvas-menu-visible')
+    main.focus()
+    return
+  }
+  if (key === 'tab' && screen.width <= 480) {
+    const anchorsArray = [...offCanvasMenu.querySelectorAll('a')]
+    const lastAnchor = anchorsArray.slice(-1)[0]
+    const firstAnchor = anchorsArray[0]
+    if (e.target === lastAnchor) {
+      firstAnchor.focus()
+      e.preventDefault()
+    }
+  }
+})
+
+document.addEventListener('focusin', (e) => {
+  console.log(e.target)
 })
